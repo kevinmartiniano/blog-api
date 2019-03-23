374b144d1d811e46027086556ad009a41d6163a8
kevinmartiniano <kevin.martiniano123@gmail.com>
2019-03-22 22:10:26 -0300

    adding store function in controller posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -33,9 +33,9 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, Post $post)
     {
-        //
+        return $post->create($request->all());
     }
 
     /**
