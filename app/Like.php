6f731393be6f48aedeca0d007ee671eec8455378
kevinmartiniano <kevin.martiniano123@gmail.com>
2019-03-22 22:53:36 -0300

    adding a fields to mass update in Like model

--- app/Like.php
+++ app/Like.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Like extends Model
 {
-    //
+    protected $fillable = [
+        'like',
+        'created_id',
+        'post_id',
+        'comment_id',
+    ];
 }
