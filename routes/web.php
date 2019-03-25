be514beb5de69be86977b861b3366e42a1f29ba8
kevinmartiniano <kevin.martiniano123@gmail.com>
2019-03-25 12:49:21 -0300

    adding route to test template home-page

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,15 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', function() {
+    // return view('pages/home-page');
+    return view('layouts/pages/home-page');
+});
