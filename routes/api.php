963f67a9b6ad011288104f436559b06b9a41b509
kevinmartiniano <kevin.martiniano123@gmail.com>
2019-03-24 21:08:45 -0300

    Adding authentication in application

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('v1')->group(function () {
+Route::middleware("auth:api")->prefix('v1')->group(function () {
     Route::resources([
         'posts' => 'PostController',
         'comments' => 'CommentController',
