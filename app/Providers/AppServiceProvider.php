1c12f5a0b4be33916192b6acce58a218b49976ba
kevinmartiniano <kevin.martiniano123@gmail.com>
2019-03-24 21:20:03 -0300

    adding configurations passport

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Laravel\Passport\Passport;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -25,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        $this->registerPolicies();
+
+        Passport::routes();
     }
 }
