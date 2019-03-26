bc5fab8eb4c45d642f43dfcc8c955e825e8d0d76
kevinmartiniano <kevin.martiniano123@gmail.com>
2019-03-26 15:10:36 -0300

    email template

--- resources/views/emails/contact/admin.blade.php
+++ resources/views/emails/contact/admin.blade.php
@@ -8,5 +8,28 @@
 		Mail sended in contact form
 	</h5>
 </p>
+
 <hr />
 
+<p>
+	O leitor {{ $contact->name }} entrou em contato pelo formulário do site.
+</p>
+
+<hr />
+
+<p>
+	Telefone para contato: {{ $contact->phone_number }}
+</p>
+<p>
+	Email para contato: {{ $contact->email }}
+</p>
+
+<hr />
+
+<p>
+	Mensagem enviada:
+</p>
+
+<p>
+	{{ $contact->message }}
+</p>
