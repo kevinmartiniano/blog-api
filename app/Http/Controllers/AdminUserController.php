ff912fe478feeaf8b5fb60602718143d028c8389
kevinmartiniano <kevin.martiniano123@gmail.com>
2019-05-09 12:23:40 -0300

    creating a method delete user in AdminUserController.

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -84,8 +84,10 @@ class AdminUserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+
+        return $user;
     }
 }
